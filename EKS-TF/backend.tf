955ef159fcf815bfecbeae8f79072c090385d5b1
dwithitechnologies <76869136+dwithitechnologies@users.noreply.github.com>
2024-02-02 23:09:08 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,10 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "my-ews-baket1a"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
   }
   required_version = ">=0.13.0"
   required_providers {
